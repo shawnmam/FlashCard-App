c9e0c799872e6cdb2857b46e165df9cddf31a4f2
Shawn <shawnmam@buffalo.edu>
2018-10-13 21:57:24 -0400

    the question and answer change visiblity

--- app/src/main/java/com/flashcards/shawnsflashcards/FlashCards.java
+++ app/src/main/java/com/flashcards/shawnsflashcards/FlashCards.java
@@ -1,7 +1,8 @@
 package com.flashcards.shawnsflashcards;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class FlashCards extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class FlashCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_cards);
+
+
+        // Makes the answer invisible in the beginning
+        findViewById(R.id.flash_card_answer).setVisibility(View.INVISIBLE);
+
+
+        findViewById(R.id.flash_card_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Makes the question invisible and then makes the answer visible
+                findViewById(R.id.flash_card_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flash_card_answer).setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+
+
+
     }
+
+
 }
